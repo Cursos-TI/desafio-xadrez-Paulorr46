676931a1082e408fa9e108886afdcac4a3274452
Paulorr46 <155102702+Paulorr46@users.noreply.github.com>
2026-04-03 17:11:18 +0000

    Finalziando Desafio Mestre do Xadrez 2.0

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,45 @@
 #include<stdio.h>
+// funções de Recuvirvidade para os moviemntos das peças
+
+//recursividade da Torre
+
+    void moverTorre(int casas){
+        if (casas > 0){
+        printf("Direita\n");
+        moverTorre (casas - 1);
+        }
+    }
+
+//recursividade para a Rainha
+
+    void moverRainha(int casas){
+        if (casas > 0){
+            printf("Esquerda\n");
+            moverRainha(casas - 1);
+        }
+    }
+
+//recursividade para o Bispo
+    void moverBispo(int casas) {
+    if (casas > 0) {
+        for (int vertical = 0; vertical < casas; vertical++) {       // Loop externo - vertical
+            for (int horizontal = 0; horizontal < casas; horizontal++) { // Loop interno - horizontal
+                if (vertical == horizontal) {  // if para mover em passos iguais na diagonal
+                    printf("Cima, Direita\n");
+                }
+            }
+        }
+    }
+}
+
 int main(){
 
     int opcao;
     int i = 0;
-     int movimentoCavalo = 1; //variavel para controle o movimento do cavalo em L
+    int movimentoCavalo = 1; //variavel para controle o movimento do cavalo em L
     
+
+
 
     printf("Qual peça você gostaria de movimentar?\n");
     printf("1. Torre\n");
@@ -17,41 +52,30 @@ int main(){
 
     case 1:
     // Mover a Torre 5 casas para a direita
-
-    for(int i = 0; i < 5 ; i++)
-    {
-        printf("Direita\n"); // imprimi a direção do movimento
-    }
+        moverTorre(5);
 
     break;
 
     case 2:
     //Mover a Rainha 8 Casas a Esquerda
-    do{
-        printf("Esquerda\n");
-        i++;
-    }while(i < 8);
+        moverRainha(8);
+
     break;
 
     case 3:
     //Mover Bispo 5 Casas a diagonal Superior Direita
-    while(i < 5){
-        printf("Cima, Direta\n");
-        i++;
+        moverBispo(5);
         break;
-    }
 
     case 4: 
     // mover Cavalo em L
-   
-        while (movimentoCavalo > 0)
-        {
-            for(int i = 0; i < 2; i++){
-                printf("Cima\n"); //imprime 'cima' duas vezes
+        
+            for(int i = 0, movimentoCavalo = 1; movimentoCavalo > 0 && i < 2; i++, movimentoCavalo--){
+                printf("Cima\n");
+                printf("Cima\n");
+                printf("Direita\n"); // imprimee 'direita' 1 vez
             }
-                printf("Direita\n"); // imprimee 'direita' duas vezes
-                movimentoCavalo--;
-        }
+        
         break;
     
     
